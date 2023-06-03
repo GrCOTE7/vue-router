51c3382c26c45473531d4ec55f4663d9f1b3ce22
Lionel COTE <grcote7@gmail.com>
2023-06-03 16:36:36 +0200

    Feat: All destination sorted by name

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
+import Home from '@/views/Home.vue'
 import Brazil from '@/views/Brazil.vue'
 import Hawaii from '@/views/Hawaii.vue'
 import Jamaica from '@/views/Jamaica.vue'
 import Panama from '@/views/Panama.vue'
 
 const routes = [
-  { path: '/', name: 'home', component: HomeView },
+  { path: '/', name: 'home', component: Home },
   { path: '/brazil', name: 'brazil', component: Brazil },
   { path: '/hawaii', name: 'hawaii', component: Hawaii },
   { path: '/jamaica', name: 'jamaica', component: Jamaica },
---
+++ src/views/Home.vue
@@ -0,0 +1,41 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div class="home">
+    <h1>All Destinations</h1>
+    <div class="destinations">
+      <router-link v-for="dest in destinations" :key="dest.id" :to="dest.slug">
+        <h2 :title="'Id :' + dest.id">{{ dest.name }}</h2>
+        <img :src="`/images/${dest.image}`" :alt="dest.name">
+      </router-link>
+    </div>
+  </div>
+</template>
+<script setup>
+import sourceData from '@/data.json'
+
+// export default {
+//   data() {
+//     return {
+//       destinations: sourceData.destinations
+//     }
+//   }
+// }
+
+console.log(sourceData)
+const destinations = sourceData.destinations
+
+const destinationsSortedByName = destinations.sort((a, b) => {
+    const nameA = a.name.toLowerCase();
+    const nameB = b.name.toLowerCase();
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    return 0;
+  });
+
+console.log(typeof(destinationsSortedByName))
+
+</script>
