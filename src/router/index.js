f5efac7216f772abdea38ef7a4325b4d34be06a0
Lionel COTE <grcote7@gmail.com>
2023-06-03 17:59:03 +0200

    Fix: Use @ for routing

--- src/router/index.js
+++ src/router/index.js
@@ -3,17 +3,17 @@ import Home from '@/views/Home.vue'
 
 const routes = [
   { path: '/', name: 'home', component: Home },
-  { path: '/brazil', name: 'brazil', component: () => import('../views/Brazil.vue') },
-  { path: '/hawaii', name: 'hawaii', component: () => import('../views/Hawaii.vue') },
-  { path: '/jamaica', name: 'jamaica', component: () => import('../views/Jamaica.vue') },
-  { path: '/panama', name: 'panama', component: () => import('../views/Panama.vue') },
+  { path: '/brazil', name: 'brazil', component: () => import('@/views/Brazil.vue') },
+  { path: '/hawaii', name: 'hawaii', component: () => import('@/views/Hawaii.vue') },
+  { path: '/jamaica', name: 'jamaica', component: () => import('@/views/Jamaica.vue') },
+  { path: '/panama', name: 'panama', component: () => import('@/views/Panama.vue') },
 
   {
     path: '/about', name: 'about',
     // route level code-splitting
     // this generates a separate chunk (About.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import('../views/About.vue')
+    component: () => import('@/views/About.vue')
   }
 ]
 
