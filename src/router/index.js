ccfb266eb0cd60021099ae5bdf8dd8ccf381be44
Lionel COTE <grcote7@gmail.com>
2023-06-03 17:56:31 +0200

    Fix: No dynamic load for homepage

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from '@/views/Home.vue'
 
 const routes = [
-  { path: '/', name: 'home', component: () => import('../views/Home.vue') },
+  { path: '/', name: 'home', component: Home },
   { path: '/brazil', name: 'brazil', component: () => import('../views/Brazil.vue') },
   { path: '/hawaii', name: 'hawaii', component: () => import('../views/Hawaii.vue') },
   { path: '/jamaica', name: 'jamaica', component: () => import('../views/Jamaica.vue') },
