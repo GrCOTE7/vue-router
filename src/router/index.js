c53e8342be49df4fbd2c6a43bc91abc514f47f19
Lionel COTE <grcote7@gmail.com>
2023-06-04 09:50:22 +0200

    Feat: Set CSS for navbar links

--- src/router/index.js
+++ src/router/index.js
@@ -20,6 +20,7 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+  linkActiveClass: 'vue-class-active-link',
 })
 
 export default router
